558104ca4d2d1f7be7136791794f9161be2866ce
Habip Akyol <habipakyol003@gmail.com>
2023-10-01 22:49:22 +0300

    no message

---
+++ Assets/Scripts/Managers/InputManager.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Data.UnityObjects;
+using Data.ValueObjects;
+using Signals;
+using Unity.Mathematics;
+using UnityEngine;
+
+namespace Managers
+{
+    public class InputManager : MonoBehaviour
+    {
+        private InputData _data;
+        private bool _isAvailableForTouch, _isFirstTimeTouchTaken, _isTouching;
+
+        private float _currentVelocity;
+        private float3 _moveVector;
+        private Vector2? _mousePosition;
+
+
+        private void Awake()
+        {
+            _data = GetInputData();
+        }
+
+        private InputData GetInputData()
+        {
+            return Resources.Load<CD_Input>("Data/CD_Input").Data;
+        }
+
+        private void OnEnable()
+        {
+            SubscribeEvents();
+        }
+
+        private void SubscribeEvents()
+        {
+            CoreGameSignals.Instance.onReset += Onreset;
+            InputSignals.Instance.OnEnableInput += OnEnableInput;
+            InputSignals.Instance.OnDisableInput += onDisableInput;
+        }
+
+        private void onDisableInput()
+        {
+            _isAvailableForTouch = false;
+        }
+
+        private void OnEnableInput()
+        {
+            _isAvailableForTouch = true;
+        }
+
+        private void Onreset()
+        {
+            _isAvailableForTouch = false;
+            _isTouching = false;
+            //_isFirstTimeTouchTaken = false;
+        }
+        
+        private void UnSubscribeEvents()
+        {
+            CoreGameSignals.Instance.onReset -= Onreset;
+            InputSignals.Instance.OnEnableInput -= OnEnableInput;
+            InputSignals.Instance.OnDisableInput -= onDisableInput;
+        }
+
+        private void OnDisable()
+        {
+            UnSubscribeEvents();
+        }
+    }
+    
+}
---
+++ Assets/Scripts/Signals/InputSignals.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+
+namespace Signals
+{
+    public class InputSignals : MonoBehaviour
+    {
+        public static InputSignals Instance;
+
+        private void Awake()
+        {
+            if (Instance!=null && Instance != this)
+            {
+                Destroy(gameObject);
+                return;
+            }
+
+            Instance = null;
+        }
+        
+        public UnityAction onFirstTimeTouchTaken = delegate{};
+        public UnityAction OnEnableInput = delegate{};
+        public UnityAction OnDisableInput = delegate{};
+        public UnityAction onInputTaken = delegate{};
+        public UnityAction onInputReleased = delegate{};
+        public UnityAction<HorizontalInputParams> onInputDragged = delegate{};
+    }
+    
+}
